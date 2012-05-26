e7083b98f13b95db849a2b857e74ad76f2d46f8b
Dennis <shr3kst3r@gmail.com>
2012-05-25 21:41:09 -0500

    Add metadata.json and bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "shr3kst3r@gmail.com"
 license          "mit"
 description      "Chef cookbook for logster"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
 
 depends "git"
 supports "ubuntu"
