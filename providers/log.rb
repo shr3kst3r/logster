035c7cb97194d76a5b15d1b57de6967ea0712a6d
Gregoire Seux <g.seux@criteo.com>
2013-09-03 13:31:39 +0200

    Add new option + fix path

--- providers/log.rb
+++ providers/log.rb
@@ -4,6 +4,7 @@ action :add do
     options = Hash.new
 
     new_resource.prefix && options["--metric-prefix"] = new_resource.prefix
+    new_resource.suffix && options["--metric-suffix"] = new_resource.suffix
     new_resource.parser_options && options["--parser-options"] = new_resource.parser_options
     new_resource.gmetric_options && options["--gmetric-options"] = new_resource.gmetric_options
     new_resource.graphite_host && options["--graphite-host"] = new_resource.graphite_host
@@ -13,7 +14,7 @@ action :add do
     o = options.map{|k,v| "#{k}=#{v}"}.join(" ")
 
     cron "logster #{new_resource.log_file.gsub(/\//, '_')}" do
-        command "/usr/sbin/logster #{o} #{new_resource.parser} #{new_resource.log_file}"
+        command "/usr/bin/logster #{o} #{new_resource.parser} #{new_resource.log_file}"
         action :create
     end
 end
--- resources/log.rb
+++ resources/log.rb
@@ -4,6 +4,7 @@ default_action :add
 
 attribute :log_file, :kind_of => String, :name_attribute => true
 attribute :prefix, :kind_of => String
+attribute :suffix, :kind_of => String
 attribute :parser_options, :kind_of => String
 attribute :gmetric_options, :kind_of => String
 attribute :graphite_host, :kind_of => String
