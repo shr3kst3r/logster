7e8e3eb4dccfb85ebdafb3b3d8c96741e46cb179
Gregoire Seux <g.seux@criteo.com>
2013-11-04 18:24:13 +0100

    Fix package installation
    When using directly a repository providing logster we should not
    download a rpm manually.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "shr3kst3r@gmail.com"
 license          "mit"
 description      "Chef cookbook for logster"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.4"
+version          "0.0.5"
 
 supports "ubuntu"
 supports 'debian'
--- recipes/install_package.rb
+++ recipes/install_package.rb
@@ -1,13 +1,15 @@
 psource = File.join(Chef::Config[:file_cache_path], "logster.rpm")
 
-remote_file psource do
-  source node[:logster][:package][:source]
-  checksum node[:logster][:package][:checksum]
-  only_if node[:logster][:package][:source]
-  notifies :install, "package[logster]", :immediately
+if node[:logster][:package][:source]
+  remote_file psource do
+    source node[:logster][:package][:source]
+    checksum node[:logster][:package][:checksum]
+    notifies :install, "package[logster]", :immediately
+  end
 end
 
+
 package "logster" do
-  action :nothing
-  source psource
+  action :nothing if node[:logster][:package][:source]
+  source psource if node[:logster][:package][:source]
 end
