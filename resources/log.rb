76e7493e433b72a45f967387be2a680dff1860b0
René Scheibe <rene.scheibe@gmail.com>
2013-12-20 17:50:49 +0100

    made 'parser' option mandatory for the 'log' resource
    - a parser is always required for logster

--- resources/log.rb
+++ resources/log.rb
@@ -1,7 +1,6 @@
 actions :add, :remove
 default_action :add
 
-
 attribute :log_file, :kind_of => String, :name_attribute => true
 attribute :prefix, :kind_of => String
 attribute :suffix, :kind_of => String
@@ -10,5 +9,5 @@ attribute :gmetric_options, :kind_of => String
 attribute :graphite_host, :kind_of => String
 attribute :state_dir, :kind_of => String
 attribute :output, :kind_of => String
-attribute :parser, :kind_of => String
+attribute :parser, :kind_of => String, :required => true
 attribute :frequency, :kind_of => Integer, :default => 1
